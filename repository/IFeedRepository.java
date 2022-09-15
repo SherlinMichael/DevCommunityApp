37c76cad94a5b446a72a9078ac4522190979ff4c
BalaGokul26 <97536309+BalaGokul26@users.noreply.github.com>
2022-09-15 17:22:22 +0530

    Delete repository directory

--- repository/IFeedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.DevCommunityApp.App.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.DevCommunityApp.App.entity.Feed;
-@Repository
-public interface IFeedRepository extends JpaRepository<Feed, Long>{
-
-}
