05d34f256c47c3307f867fd55836abff8c055011
Sandhya-G-2020 <71721402+Sandhya-G-2020@users.noreply.github.com>
2022-09-19 09:32:07 +0530

    Create UserConfiguration.java

---
+++ config/UserConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.emp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class UserConfiguration {
+	
+	@Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	
+}
